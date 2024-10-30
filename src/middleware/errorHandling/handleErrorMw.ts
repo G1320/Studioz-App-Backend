aa66c26f94cad11b21b4f96535f5ce3cab49ed26
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-10-30 10:42:32 +0200

    Temp remove token varification from cart routes

--- src/middleware/errorHandling/handleErrorMw.ts
+++ src/middleware/errorHandling/handleErrorMw.ts
@@ -1,10 +1,15 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 interface ErrorWithStatusCode extends Error {
   statusCode?: number;
 }
 
-const handleErrorMw = (err: ErrorWithStatusCode, req: Request, res: Response): void => {
+const handleErrorMw = (
+  err: ErrorWithStatusCode,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
   const { statusCode, message } = err;
   console.error(message);
   res.status(statusCode || 500).send(message || 'Something went wrong!');
